64d46e087f4a633d72cae4dc24d71b57f6e6a046
unknown <jedelrio@697-334-Andunie.sf.k12.internal>
2014-12-17 07:54:18 -0800

    Google problem

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -16,17 +16,39 @@ public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
-{            
-     noLoop();  
+{   
+	for(int i = 1; i < e.length()-10; i++){
+		String digits = e.substring(i, i+11);
+		double convert = Double.parseDouble(digits); 
+			//System.out.println(convert);   
+
+		if(isPrime(convert) == true){
+			System.out.println(convert);
+			break;
+		}
+	} 
+	
+	 
+    noLoop();  
+
 }  
 public void draw()  
 {   
 	//not needed for this assignment
+	
+
 }  
-public boolean isPrime(double dNum)  
+public boolean isPrime(double convert1)  
 {   
-    //to be finished later   
-    return true;  
+    //to be finished later
+
+    for(int i = 2; i < Math.sqrt(convert1)+1; i++){
+    	if(convert1 % i == 0){
+    		return false;
+    	}	
+	}
+    	return true;  
+
 } 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
